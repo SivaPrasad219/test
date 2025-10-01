fc8f2f756ba8e4150300423512ac28d05edfda56
Sivaprasad Tellagorla <sivaprasad@buildbot.tech>
2025-10-01 11:09:49 +0530

    32

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-essential",
+    "@vue/eslint-config-typescript",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "plugin:prettier/recommended",
